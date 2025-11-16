160adcbff47e30fd453b89943c4793799b812198
105615691 <105615691@omidtech-co.local>
2025-11-16 15:46:22 +0330

    MudImage in grid

--- KomaeiSample.Share/Dto/ProductDto.cs
+++ KomaeiSample.Share/Dto/ProductDto.cs
@@ -3,10 +3,9 @@
 public class ProductDto
 {
     public int Id { get; set; }
-
     public string CategoryTitle { get; set; } = null!;
-
     public string Title { get; set; } = null!;
-
     public decimal Price { get; set; }
+    public string FileName { get; set; } = null!;
+    public string FileExtension { get; set; } = null!;
 }
